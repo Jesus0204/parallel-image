3281cda22c9482a22c7d517f3f1a7a9821aeb2cb
Jesus0204 <a01705442@tec.mx>
2024-05-19 14:20:46 -0600

    Finish front-end of homepage

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -1,5 +1,20 @@
-exports.processImage = (request, response, next) => {
+// To use the parallel library
+const parallel = require('paralleljs');
 
+exports.processImage = (request, response, next) => {
+    if (request.body.type = 'blur'){
+        console.log('blur');
+    } else if (request.body.type = 'grayscale') {
+        console.log('grayscale');
+    } else if (request.body.type = 'sepia') {
+        console.log('sepia');
+    } else if (request.body.type = 'invert') {
+        console.log('invert');
+    }
+    // For the time being so the website doesn't load forever
+    response.render('homePage', {
+        csrfToken: request.csrfToken(),
+    });
 };
 
 exports.getHomePage = (request, response, next) => {
