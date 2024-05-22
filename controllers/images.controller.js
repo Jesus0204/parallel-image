3613440c15b3285f0b47104ec3ada6eecc755032
Jesus0204 <a01705442@tec.mx>
2024-05-21 21:27:33 -0600

    Add the option to select workers

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -20,8 +20,7 @@ function createWorker(pixels, type) {
     });
 }
 
-async function imagePartsPreProcessing(pixels, type){
-    const numOfThreads_CPU = os.cpus().length;; // Number of cores to use
+async function imagePartsPreProcessing(pixels, type, numOfThreads_CPU) {
     // Ensure that the image part size is divisible by 4 so the algorithm works correctly
     const imageSizepPart = Math.ceil(pixels.length / numOfThreads_CPU / 4) * 4;
     const parts = [];
@@ -41,59 +40,66 @@ async function imagePartsPreProcessing(pixels, type){
         let end = moment();
     
         // Get the time it took
+        let timeTook = end - start;
         console.log(end - start + ' ms');
 
-        return transformedImage;
+        return {transformedImage, timeTook};
     } catch(error) {
         console.log(error);
-        return [];
+        let emptyArray = [];
+        return {emptyArray, timeTook};
     }
 
 }
 
 exports.processImage = async (request, response, next) => {
 
+    let numOfThreads_CPU;
+    if (request.body.cores == 'single'){
+        numOfThreads_CPU = 1;
+    } else if (request.body.cores == 'multiple') {
+        numOfThreads_CPU = os.cpus().length;; // Cores of the user's computer
+    }
+
     if (request.file.mimetype == 'image/png'){ 
         // Using png.js parse through the PNG (which I can get as the buffer)
         var reader = new PNGReader(request.file.buffer);
         reader.parse(async function (error, png) {
+            // Get the pixels and the properties of the PNG image
+            let pixels = Array.from(png.pixels);
+            let width = png.width;
+            let height = png.height;
+            
             try {
-                // Get the pixels and the properties of the PNG image
-                let pixels = Array.from(png.pixels);
-                let width = png.width;
-                let height = png.height;
-                
-                try {
-                    let transformedImage = await imagePartsPreProcessing(pixels, request.body.type);
+                let {
+                    transformedImage,
+                    timeTook
+                } = await imagePartsPreProcessing(pixels, request.body.type, numOfThreads_CPU);
+    
+                // This part of the code was generated with ChatGTP, since I had no knowledge of how to recontruct the image
+                // Reconstruct the processed pixels
+                const newPixels = Buffer.concat(transformedImage.map(Buffer.from));
+    
+                const newPng = new PNG({
+                    width,
+                    height
+                });
+                newPng.data = newPixels;
         
-                    // This part of the code was generated with ChatGTP, since I had no knowledge of how to recontruct the image
-                    // Reconstruct the processed pixels
-                    const newPixels = Buffer.concat(transformedImage.map(Buffer.from));
+                // Convert the new PNG image to a buffer and then to a Base64 string
+                const chunks = [];
+                newPng.pack().on('data', chunk => chunks.push(chunk)).on('end', () => {
+                    const buffer = Buffer.concat(chunks);
+                    const base64Image = buffer.toString('base64');
+                    const dataUri = `data:image/png;base64,${base64Image}`;
         
-                    const newPng = new PNG({
-                        width,
-                        height
+                    // Render the new EJS with the newly formed image
+                    response.render('processedImage', {
+                        image: dataUri,
+                        time: timeTook,
+                        workers: numOfThreads_CPU
                     });
-                    newPng.data = newPixels;
-            
-                    // Convert the new PNG image to a buffer and then to a Base64 string
-                    const chunks = [];
-                    newPng.pack().on('data', chunk => chunks.push(chunk)).on('end', () => {
-                        const buffer = Buffer.concat(chunks);
-                        const base64Image = buffer.toString('base64');
-                        const dataUri = `data:image/png;base64,${base64Image}`;
-            
-                        // Render the new EJS with the newly formed image
-                        response.render('processedImage', {
-                            image: dataUri
-                        });
-                    });
-                } catch (error) {
-                    console.log(error);
-                    response.status(500).render('500', {
-                        filetype: false
-                    });
-                }
+                });
             } catch (error) {
                 console.log(error);
                 response.status(500).render('500', {
@@ -111,7 +117,10 @@ exports.processImage = async (request, response, next) => {
         height = jpegData.height;
 
         try {
-            let transformedImage = await imagePartsPreProcessing(pixels, request.body.type);
+            let {
+                transformedImage,
+                timeTook
+            } = await imagePartsPreProcessing(pixels, request.body.type, numOfThreads_CPU);
 
             // Rebuild the JPEG Image with the library functions
             const newPixels = new Uint8Array(pixels.length);
@@ -133,7 +142,9 @@ exports.processImage = async (request, response, next) => {
 
              // Render the new EJS with the newly formed image
              response.render('processedImage', {
-                 image: dataUri
+                 image: dataUri, 
+                 time: timeTook, 
+                 workers: numOfThreads_CPU
              });
 
         } catch (error) {
